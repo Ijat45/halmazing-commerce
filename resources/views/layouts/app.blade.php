8280d4e09b160e7f210d32b2e7c54339ea551d34
izzat <muhdizzat0923@gmail.com>
2025-11-18 16:57:51 +0800

    feat(app.blade.php): add CSRF Token meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>Halmazing Commerce</title>
     <meta name="description" content="">
     <meta name="keywords" content="">
